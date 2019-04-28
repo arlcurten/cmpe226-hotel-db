9b0fd9a11da8bb7813910d1b60cc84d34be44ab3
Taichun Wei <taichun.wei@gokeuslab.com>
2019-04-28 15:00:06 -0700

    hotel.sql

--- SQL/hotel.sql
+++ SQL/hotel.sql
@@ -7,15 +7,15 @@ CREATE TABLE Guest(
     guestID VARCHAR(20) NOT NULL, -- PK
     guestFName VARCHAR(20),
     guestLName VARCHAR(20),
-    guestPhoneNumber CHAR(10),
-    guestPassword VARCHAR(20) NOT NULL -- VARHCAR size depending on the encrpytion 
+    guestPhoneNumber CHAR(10)
+    -- guestPassword VARCHAR(20) NOT NULL -- VARHCAR size depending on the encrpytion  -- Dave:let's add it later
 );
 
 CREATE TABLE Invoice(
     invoiceNum INT NOT NULL, -- PK
     invoiceDate DATE,
-    roomCharge DECIMAL(4,2),
-    foodCharge DECIMAL(4,2),
+    roomCharge DECIMAL(6,2),
+    foodCharge DECIMAL(6,2),
     guestID VARCHAR(20) --  FK to Guest.guestID
 );
 
@@ -30,13 +30,19 @@ CREATE TABLE Reservation(
 CREATE TABLE Room(
     roomNumber INT NOT NULL, -- PK
     roomType CHAR(3) NOT NULL, -- 'std':standard, 'lux':luxury, 'sui':suite
-    roomPrice DECIMAL(4,2),
+    roomPrice DECIMAL(6,2),
     roomStatus INT -- empty:0, occupied=1, cleaning=2, construction=3
 );
 
 CREATE TABLE Orders(
     orderNumber INT NOT NULL, -- PK
     guestID VARCHAR(20) -- FK to Guest.guestID
+);
+
+CREATE TABLE Produces(
+    orderNumber INT NOT NULL, -- PK & FK to Orders.orderNumber
+    foodID INT NOT NULL, -- PK & FK to Food.foodID
+    quantity INT NOT NULL
 );
 
 CREATE TABLE Food(
@@ -51,10 +57,10 @@ CREATE TABLE Employee(
     employeeSSN CHAR(9) NOT NULL, -- PK (reflect this change on ERD)
     employeeFName VARCHAR(20) NOT NULL,
     employeeLName VARCHAR(20) NOT NULL,
-    employeeSex CHAR(1), -- M: men, W:women
+    -- employeeSex CHAR(1), -- M: men, W:women  -- Dave:could we remove the attribute?
     employeeDOB DATE,
     employeeSalary INT,
-    employeePassword VARCHAR(20) NOT NULL, -- depending on  the encryption
+    -- employeePassword VARCHAR(20) NOT NULL, -- depending on  the encryption  -- Dave:let's add it later
     dno INT NOT NULL -- FK to Department.departmentNumber
 );
 
@@ -70,14 +76,19 @@ ALTER TABLE Invoice ADD CONSTRAINT PK_Invoicec_Num PRIMARY KEY(invoiceNum);
 ALTER TABLE Reservation ADD CONSTRAINT PK_Reservation_Num PRIMARY KEY(bookNumber);
 ALTER TABLE Room ADD CONSTRAINT PK_Room_Num PRIMARY KEY(roomNumber);
 ALTER TABLE Orders ADD CONSTRAINT PK_Order_Num PRIMARY KEY(orderNumber);
+ALTER TABLE Produces ADD CONSTRAINT PK_Produce_orNum_fID PRIMARY KEY(orderNumber, foodID);
 ALTER TABLE Food ADD CONSTRAINT PK_Food_ID PRIMARY KEY(foodID);
 ALTER TABLE Employee ADD CONSTRAINT PK_Employee_SSN PRIMARY KEY(employeeSSN);
 ALTER TABLE Department ADD CONSTRAINT PK_Department_Num PRIMARY  KEY(departmentNumber);
 
 -- assigning FK to each table --
-ALTER TABLE Invoice ADD CONSTRAINT FK_Invoice_Guest_ID FOREIGN KEY(guestID) REFERENCES Guest(guestID);
-ALTER TABLE Reservation ADD CONSTRAINT FK_Reservation_Guest_ID FOREIGN KEY(guestID) REFERENCES Guest(guestID);
-ALTER TABLE Reservation ADD CONSTRAINT FK_Room_Num FOREIGN KEY(roomNum) REFERENCES Room(roomNumber);
-ALTER TABLE Orders ADD CONSTRAINT FK_Orders_Guest_ID FOREIGN KEY(guestID) REFERENCES Guest(guestID);
-ALTER TABLE Employee ADD CONSTRAINT FK_Employee_Dept_Num FOREIGN KEY(dno) REFERENCES Department(departmentNumber);
-ALTER TABLE Department ADD CONSTRAINT FK_Manager_SSN FOREIGN KEY(manager_SSN) REFERENCES Employee(employeeSSN);
+/*
+ALTER TABLE Employee ADD CONSTRAINT FK_Employee_Dept_Num FOREIGN KEY(dno) REFERENCES Department(departmentNumber)  ON DELETE CASCADE  ON UPDATE CASCADE;
+ALTER TABLE Department ADD CONSTRAINT FK_Manager_SSN FOREIGN KEY(manager_SSN) REFERENCES Employee(employeeSSN)  ON DELETE CASCADE  ON UPDATE CASCADE;
+ALTER TABLE Invoice ADD CONSTRAINT FK_Invoice_Guest_ID FOREIGN KEY(guestID) REFERENCES Guest(guestID)  ON DELETE SET NULL  ON UPDATE CASCADE;
+ALTER TABLE Reservation ADD CONSTRAINT FK_Reservation_Guest_ID FOREIGN KEY(guestID) REFERENCES Guest(guestID)  ON DELETE CASCADE  ON UPDATE CASCADE;
+ALTER TABLE Reservation ADD CONSTRAINT FK_Room_Num FOREIGN KEY(roomNum) REFERENCES Room(roomNumber)  ON DELETE CASCADE  ON UPDATE CASCADE;
+ALTER TABLE Orders ADD CONSTRAINT FK_Orders_Guest_ID FOREIGN KEY(guestID) REFERENCES Guest(guestID)  ON DELETE CASCADE  ON UPDATE CASCADE;
+ALTER TABLE Produces ADD CONSTRAINT FK_Produces_orderNumber FOREIGN KEY(orderNumber) REFERENCES Orders(orderNumber)  ON DELETE CASCADE  ON UPDATE CASCADE;
+ALTER TABLE Produces ADD CONSTRAINT FK_Produces_foodID FOREIGN KEY(foodID) REFERENCES Food(foodID)  ON DELETE CASCADE  ON UPDATE CASCADE;
+*/
